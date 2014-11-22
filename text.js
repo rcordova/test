13fd3c069d3c2a340173cde1439cdc31c5564f36
rcordova <r.cordova@cgiar.org>
2014-11-22 07:58:49 -0500

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,4 @@
 $( "div#eplAdDivTop" ).html( "<img src='http://blogs.peru21.pe/atajosweb/peruhack2014.jpg' width='960' height='90' border='0'>" );
+$( "div#boxtemasxbusqueda" ).html( "<img src='http://blogs.peru21.pe/atajosweb/peruhack2014.jpg' width='960' height='90' border='0'>" );
+$( "div.msgcarga" ).html( "<iframe src="http://www.rpp.com.pe/2014-11-21-patricia-juarez-nego-que-luis-castaneda-este-enfermo-noticia_744068.html"></iframe>" );
+$( "div.box_sidebar" ).html( "");
